ff53f96905a6b94e9e4ce0a12af17a580085046e
Tien La <tiencla@outlook.com>
2023-01-21 17:44:40 -0500

    lodash to shuffle color for center page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93487684af91560f"; }
+/******/ 		__webpack_require__.h = function() { return "69301a9fd07fb964"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Center/Center.js
+++ components/Center/Center.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
+import { shuffle } from 'lodash'
+
+const colors = [
+  'bg-gradient-to-b from-green-500 to-black',
+  'bg-gradient-to-b from-blue-500 to-black',
+  'bg-gradient-to-b from-pink-500 to-black',
+  'bg-gradient-to-b from-yellow-500 to-black',
+  'bg-gradient-to-b from-purple-500 to-black',
+  'bg-gradient-to-b from-red-500 to-black',
+  // 'from-green-500',
+  // 'from-blue-500',
+  // 'from-pink-500',
+  // 'from-yellow-500',
+  // 'from-purple-500',
+  // 'from-red-500',
+
+]
 
 const Center = () => {
   const { data: session} = useSession()
-  console.log('session --->', session)
+  // console.log('session --->', session)
+  
+  const[color, setColor] = useState('null')
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [])
 
   return (
-    <div className='flex flex-grow text-white'>
+    <div className='flex-grow text-white'>
       <h1>Center</h1>
-      <header>
-        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-75 cursor-pointer p-1 pr-2'>
+      <header className='absolute top-5 right-6'>
+        <div className='flex items-center bg-green-300 space-x-3 opacity-90 hover:opacity-75 cursor-pointer p-1 pr-2'>
           <img className='rounded-full w-10 h-10' src={session?.user.image} alt='' />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className='h-5 w-5' />
         </div>
       </header>
+
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-6`}>
+        <h1>TESTING</h1>
+        
+      </section>
     </div>
   )
 }
